2c890c5ca8b69a17937ecc38707d018c176966ab
Rich Johnson <webaddict@gmail.com>
2016-05-26 14:45:35 -0700

    published 1.0.2 to android play store

--- build-android.bat
+++ build-android.bat
@@ -6,5 +6,5 @@ ionic build --release android
 
 "C:\Program Files\Java\jdk1.8.0_45\bin\jarsigner.exe" -tsa http://timestamp.digicert.com -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore "C:\apps\LDSFight\.private\LDSFIGHT.keystore" -storepass 41f6nGH258jk9104prp -keypass 41f6nGH258jk9104prp "C:\apps\LDSFight\platforms\android\build\outputs\apk\android-release-unsigned.apk" LDSFight
 
-"C:\Development\adt-bundle\sdk\build-tools\22.0.1\zipalign.exe" -f -v 4 "C:\apps\LDSFight\platforms\android\build\outputs\apk\android-release-unsigned.apk" "C:\apps\LDSFight\platforms\android\build\outputs\apk\LDSFight-1.0.2.apk"
+"C:\Development\adt-bundle\sdk\build-tools\22.0.1\zipalign.exe" -f -v 4 "C:\apps\LDSFight\platforms\android\build\outputs\apk\android-release-unsigned.apk" "C:\apps\LDSFight\platforms\android\build\outputs\apk\LDSFight-1.0.3.apk"
 
--- www/js/directives.js
+++ www/js/directives.js
@@ -4,25 +4,22 @@ angular.module('app.directives', [])
 
 }])
 
-.directive('countdown', [
-	'Util',
-	'$interval',
-	function (Util, $interval) {
-		return {
-			restrict: 'A',
-			scope: { date: '@' },
-			link: function (scope, element) {
-				var future;
-				future = new Date(scope.date);
-				$interval(function () {
-					var diff;
-					diff = Math.floor((future.getTime() - new Date().getTime()) / 1000);
-					return element.text(Util.dhms(diff));
-				}, 1000);
-			}
-		};
-	}
-])
+.directive('countdown', ['Util', '$interval', function (Util, $interval) {
+	return {
+		restrict: 'A',
+		scope: { date: '@' },
+		link: function (scope, element) {
+			var future;
+			future = new Date(scope.date);
+			$interval(function () {
+				var diff;
+				diff = Math.floor((future.getTime() - new Date().getTime()) / 1000);
+				return element.text(Util.dhms(diff));
+			}, 1000);
+		}
+	};
+}])
+
 .factory('Util', [function () {
 	return {
 		dhms: function (t) {
